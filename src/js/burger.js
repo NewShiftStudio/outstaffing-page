128a4079aaded3463d133498d01e1564eb4a8822
Eeveeee <fx8300gfe@gmail.com>
2021-04-15 13:21:57 +0300

    fixes

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,10 +2,13 @@ const burgerBtn = document.querySelector('.btn-burger')
 const burgerMenu = document.querySelector('.header-navbar-items')
 const navigation = document.querySelectorAll('.header-navbar__item')
 
+const form = document.querySelector('.request-form-scroll')
+const toFormBtn = document.querySelector('.btn-offer')
+
 document.body.style.overflowY = 'visible'
 
-navigation.forEach(selector => {
-  selector.addEventListener('click', e => {
+navigation.forEach((selector) => {
+  selector.addEventListener('click', (e) => {
     e.preventDefault()
     let equal = document.querySelector(`.${selector.getAttribute('href')}`)
     equal.scrollIntoView({ block: 'start', behavior: 'smooth' })
@@ -16,8 +19,11 @@ navigation.forEach(selector => {
   })
 })
 
-burgerBtn.addEventListener('click', e => {
+burgerBtn.addEventListener('click', (e) => {
   e.preventDefault()
   burgerBtn.classList.toggle('active')
   burgerMenu.classList.toggle('active')
+})
+toFormBtn.addEventListener('click', (e) => {
+  form.scrollIntoView({ block: 'center', behavior: 'smooth' })
 })
